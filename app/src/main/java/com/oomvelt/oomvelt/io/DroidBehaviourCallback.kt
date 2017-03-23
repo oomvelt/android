9a732f19a05162f822fc7ece369ee16857c130b7
James Taylor <james@proxama.com>
2017-03-23 20:11:00 +0000

    Fix logging in the callback.

--- app/src/main/java/com/oomvelt/oomvelt/io/DroidBehaviourCallback.kt
+++ app/src/main/java/com/oomvelt/oomvelt/io/DroidBehaviourCallback.kt
@@ -1,13 +1,17 @@
 package com.oomvelt.oomvelt.io
+import org.jetbrains.anko.info
+import org.jetbrains.anko.AnkoLogger
 
 import raticator.RatState
 import raticator.StateChangeCallback
 
 
-class DroidBehaviourCallback : StateChangeCallback {
+class DroidBehaviourCallback : StateChangeCallback, AnkoLogger {
 
     override fun StateChanged(newState: RatState) {
         // TODO: make this do something on the UI thread now!
-        print("Rat state change has been detected: " + newState.toString())
+        info("----------------------------------------------------------")
+        info("Rat state change has been detected: " + newState.toString())
+        info("----------------------------------------------------------")
     }
 }
